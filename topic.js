ecc630e00ccf671f758999d7f6d8e944c2e8cab6
alexis 🍙 <alexis@ohagiiman.me>
2020-01-27 19:37:22 -0600

    Add package.json for easier setup

--- topic.js
+++ topic.js
@@ -2,35 +2,39 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const cheerio = require('cheerio');
-const rp = require("request-promise");
+const rp = require('request-promise');
 
 const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} is online.`);
-    client.user.setActivity(`${prefix}topic | @ohagiiman <3`);
+	console.log(`${client.user.tag} is online.`);
+	client.user.setActivity(`${prefix}topic`);
 });
 
-client.on('message', (message) => {
-    if(message.author.bot || !message.guild) return;
-    if(!message.content.toLowerCase().startsWith(prefix)) return;
+client.on('message', message => {
+	if (message.author.bot || !message.guild) return;
+	if (!message.content.toLowerCase().startsWith(prefix)) return;
 
-    const args = message.content.toLowerCase().slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
-    
-    switch(command) {
-      case 'topic': 
-        rp("https://www.conversationstarters.com/generator.php")
-           .then((html) => {
-             let $ = cheerio.load(html);
-             let text = $('#random').text();
-             text ? message.channel.send(text) : message.channel.send('Wooops! Looks like something happened while trying to retrieve a topic.');
-         });
-        break;
-      case 'ping':
-        const ping = Math.round(client.ping);
-        return message.channel.send(`Pong! | \`${ping}ms\` ${ping >= 150 ? '🐢' : '🐇'}.`);    
-    };
+	const args = message.content
+		.toLowerCase()
+		.slice(prefix.length)
+		.split(' ');
+	const command = args.shift().toLowerCase();
+
+	switch (command) {
+		case 'topic':
+			rp('https://www.conversationstarters.com/generator.php').then(html => {
+				let $ = cheerio.load(html);
+				let text = $('#random').text();
+				text
+					? message.channel.send(text)
+					: message.channel.send('Wooops! Looks like something happened while trying to retrieve a topic.');
+			});
+			break;
+		case 'ping':
+			const ping = Math.round(client.ping);
+			return message.channel.send(`Pong! | \`${ping}ms\` ${ping >= 150 ? '🐢' : '🐇'}.`);
+	}
 });
 
-client.login(token); 
+client.login(token);
